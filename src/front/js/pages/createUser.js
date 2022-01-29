b94685a3f238ff099d758ac2fe02812815410d3c
Taylor Hart <thart003@ucr.edu>
2022-01-29 02:09:51 +0000

    add /pets to single.js

--- src/front/js/pages/createUser.js
+++ src/front/js/pages/createUser.js
@@ -1,59 +1,52 @@
 import React from "react";
-import "../../styles/createUser.scss";
+import { Link, useHistory } from "react-router-dom";
+import { Context } from "../store/appContext";
+import { Alert } from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
+import "../../styles/createUser.scss";
 
+export const Register = () => {
+    const {store, actions } = useContext(Context);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [first_name, setFirst_name] = useState("");
+    const [last_name, setLast_name] = useState("");
+    const [date, setDate] = useState("");
+    const [error, setError] = useState(null);
+    const [LoginStatus, setLoginStatus] = useState("");
+    const history = useHistory();
+    const [messageState, setMessageState] = useState({
+      isActive: false,
+      message: "hello"
+    });
 
-
-<form id="createUserForm">
-    <div className="row">
-    <div className="form-group col-xs-6">
-        <label for="firstname">First Name</label>
-        <input type="text" class="form-control" id="firstName" placeholder="first name"/>
-    </div>
-    <div className="form-group col-xs-6">
-    <label for="lastname">Last Name</label>
-    <input type="text" class="form-control" id="lastName" placeholder="last name"/>
-  </div>
-  </div>
-  <div className="row">
-    <div className="form-group">
-        <label for="exampleFormControlInput1">Email</label>
-        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="email"/>
-    </div>
-  </div>
-  <div className="row">
-    <div className="form-group">
-        <label for="exampleFormControlInput1">Phone</label>
-        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="phone"/>
-    </div>
-  </div>
-  <div className="row">
-    <div className="form-group">
-        <label for="exampleFormControlInput1">Zip Code</label>
-        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="zip code"/>
-    </div>
-  </div>
-  <div className="row">
-    <div className="form-group">
-        <label for="exampleFormControlInput1">Username</label>
-        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="username"/>
-    </div>
-  </div>
-  <div className="row">
-    <div className="form-group">
-        <label for="exampleFormControlInput1">Password</label>
-        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="password"/>
-    </div>
-  </div>
-  <div class="input-group">
-  <div class="input-group-prepend">
-    <div class="input-group-text">
-    <input type="radio" aria-label="Radio button for following text input"/>
-    </div>
-  </div>
-  <input type="text" class="form-control" aria-label="Text input with radio button"/><label className="radio3">
-        I Agree with the Privacy Policy and the Terms of Service ("Agreement")
-    </label>
-</div>
-<button class="btn btn-primary" type="submit">Submit</button>
-</form>
+    const handleSubmit = e => {
+      e.preventDefault();
+      console.log(email, password, first_name, last_name, date);
+      actions 
+          .signup(email, password, first_name, last_name, date, setMessageState)
+          .then(date => history.push("/userDashboard"))
+          .catch(error => {
+            setError(error);
+            console.log("This is an error", error);
+          });
+    };
+    return (
+          <div className="registerImage vh-100">
+            <div className="container py-5 h-100">
+              <div className="row d-flex justify-content-center align-items-center h-100">
+                <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                  <div className="card-body p-5 text-center">
+                    <ul className="nav d-flex justify-content-center" id="myCatalog" role="tablist" />
+                      <form className="loginForm" onSubmit={handleSubmit}>
+                        <div>
+                          <h1 className="createUserTitle">Create User</h1>
+                        </div>
+                      </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    );
+};
