d187dae07d2efcd5ebf20d10dd67da192195ec46
diegomarteens <diegomarteens@gmail.com>
2022-01-29 17:00:34 +0000

    pet form v3

--- src/front/js/pages/form.js
+++ src/front/js/pages/form.js
@@ -8,59 +8,66 @@ export const Petsform = () => {
 					<h2>Start Your Alert Now! Its Free!!</h2>
 					<p className="maintext">Enter your PET information and spread the add throughout our community</p>
 				</div>
-
-                <div className="radiobutton">
-                    <div className="form-check">
-                        <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
-                        <label className="form-check-label" for="flexRadioDefault1">
-                            Pet Lost
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input
-                            className="form-check-input"
-                            type="radio"
-                            name="flexRadioDefault"
-                            id="flexRadioDefault2"
-                            checked
-                        />
-                        <label class="form-check-label" for="flexRadioDefault2">
-                            Pet Found
-                        </label>
-                        </div>
-                    </div>
-                </div>
 				<div className="row">
-					<div className="form-group col-xs-6">
-						<label htmlFor="name">Name</label>
-						<input type="text" className="petname" id="name" placeholder="Name" />
-					</div>
-					<div className="form-group col-xs-6">
-						<label htmlFor="color">Color</label>
-						<input type="text" clasName="petcolor" id="color" placeholder="Color" />
-					</div>
-					<div className="form-group col-xs-6">
-						<label htmlFor="height">Height</label>
-						<input type="text" clasName="petheight" id="height" placeholder="Height" />
-					</div>
-					<div className="form-group col-xs-6">
-						<label htmlFor="weight">Weight</label>
-						<input type="text" clasName="petweight" id="weight" placeholder="Weight" />
-					</div>
-					<div className="form-group col-xs-6">
-						<label htmlFor="Zipcode">Zipcode</label>
-						<input type="text" clasName="petzipcode" id="zipcode" placeholder="Zipcode" />
-					</div>
-					<div className="form-group col-xs-6">
-						<label htmlFor="species">Species</label>
-						<input type="text" clasName="petspecies" id="species" placeholder="Species" />
+					<div className="radiobutton">
+						<div className="form-check">
+							<input
+								className="form-check-input"
+								type="radio"
+								name="flexRadioDefault"
+								id="flexRadioDefault1"
+							/>
+							<label className="form-check-label" htmlFor="flexRadioDefault1">
+								Pet Lost
+							</label>
+						</div>
+						<div className="form-check">
+							<input
+								className="form-check-input"
+								type="radio"
+								name="flexRadioDefault"
+								id="flexRadioDefault2"
+								checked
+							/>
+							<label className="form-check-label" htmlFor="flexRadioDefault2">
+								Pet Found
+							</label>
+						</div>
 					</div>
 				</div>
-				<div className="uploadbutton" />
+			</div>
+			<div className="row">
+				<div className="form-group col-xs-6">
+					<label htmlFor="name">Name</label>
+					<input type="text" className="petname" id="name" placeholder="Name" />
+				</div>
+				<div className="form-group col-xs-6">
+					<label htmlFor="color">Color</label>
+					<input type="text" clasName="petcolor" id="color" placeholder="Color" />
+				</div>
+				<div className="form-group col-xs-6">
+					<label htmlFor="height">Height</label>
+					<input type="text" clasName="petheight" id="height" placeholder="Height" />
+				</div>
+				<div className="form-group col-xs-6">
+					<label htmlFor="weight">Weight</label>
+					<input type="text" clasName="petweight" id="weight" placeholder="Weight" />
+				</div>
+				<div className="form-group col-xs-6">
+					<label htmlFor="Zipcode">Zipcode</label>
+					<input type="text" clasName="petzipcode" id="zipcode" placeholder="Zipcode" />
+				</div>
+				<div className="form-group col-xs-6">
+					<label htmlFor="species">Species</label>
+					<input type="text" clasName="petspecies" id="species" placeholder="Species" />
+				</div>
+			</div>
+			<div className="uploadbutton">
 				<button className="btn btn-primary" type="submit">
 					Upload Picture
 				</button>
-				<div className="submitbutton" />
+			</div>
+			<div className="submitbutton">
 				<button className="btn btn-primary" type="submit">
 					Submit
 				</button>
