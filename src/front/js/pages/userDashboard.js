465d7e309ccc4d7feb36f381ead79980776616c0
Taylor Hart <thart003@ucr.edu>
2022-02-01 22:59:56 +0000

    starting place for dashboard

--- src/front/js/pages/userDashboard.js
+++ src/front/js/pages/userDashboard.js
@@ -9,36 +9,28 @@ export const MyPosts = props => {
   const { store, actions } = useContext(Context);
   const params = useParams();
   const [lostPet, setLostPet] = useState(null);
-  useEffect(async () => {
-    setLostPet(await actions.getSingleLostPet(params.postId));
-  }, []);
-  if (lostPet === null || lostPet === undefined){
-    return <Image className="mx-auto d-block" src="https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif" />;
-  } 
+  
   return (
-    <Container className=" profile text-center">
-      <h1 className="postTitle">{lostPet.name}</h1>
-
-      <Card className="post m-auto" style={{ width: "40rem", border: "none" }}>
-        <Card.Img variant="top" src={lostPet.image} />
-      </Card>
-
-      <hr className="line" />
-      <Row className="lostPetPostTitles">
-        <Col> Title </Col>
-      </Row>
-      <Row className="lostPetPostList">
-        <Col>{lostPet.title}</Col>
-        <Button className="mr-2" variant="dark" size="lg">
-          Delete
-        </Button>
-      </Row>
-      <Container>
-        <Button className="mr-2" href="/home" variant="dark" size="lg">
-          Home
-        </Button>
-      </Container>
-    </Container>
-
+    <ol id="horizontal-list">
+					{list.map((item, index) => {
+						return (
+							<li className="listItem" key={index}>
+								{item}
+								<button
+									className="removeBtn"
+									onClick={() => {
+										const newList = list.filter((e, i) => {
+											return i !== index;
+										});
+										setList(newList);
+									}}>
+									{" "}
+									Remove
+								</button>
+							</li>
+						);
+					})}
+					<p className="paper">{`There are ${list.length} items left`}</p>
+				</ol>
   );
 };
